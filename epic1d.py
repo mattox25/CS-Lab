ff74b49ae2e1a2293bbc7ab440440e8e9a447e54
mattox25 <jvf522@york.ac.uk>
2024-12-03 14:54:50 +0000

    modularised code

--- epic1d.py
+++ epic1d.py
@@ -7,6 +7,7 @@ from numpy import exp, sin, cos, sqrt, random, histogram, abs, sqrt, max, savetx
 import os
 
 
+
 import matplotlib.pyplot as plt # Matplotlib plotting library
 
 try:
@@ -275,43 +276,4 @@ def exponential(x, a, b):
 def linear(x, m, c):
     return m * x + c
 
-####################################################################
-
-if __name__ == "__main__":
-    # Generate initial condition
-    # 
-    npart = 20000   
-    if False:
-        # 2-stream instability
-        L = 100
-        ncells = 20
-        pos, vel = twostream(npart, L, 3.) # Might require more npart than Landau!
-    else:
-        # Landau damping
-        L = 4.*pi
-        ncells = 20
-        pos, vel = landau(npart, L)
-    
-    # Create some output classes
-    # p = Plot(pos, vel, ncells, L) # This displays an animated figure - Slow!
-    s = Summary()                 # Calculates, stores and prints summary info
-
-    diagnostics_to_run = [s]   # Remove p to get much faster code!
-
-    
-    # Run the simulation
-    pos, vel = run(pos, vel, L, ncells, 
-                   out = diagnostics_to_run,        # These are called each output step
-                   output_times=linspace(0.,20,50)) # The times to output
-    
-    # Summary stores an array of the first-harmonic amplitude
-
-
-    harmonic_data = column_stack((s.t, s.firstharmonic))
-    
-    directory = '/Users/jvf522/Documents/CS Lab/Lab1'
-    if not os.path.exists(directory):
-        os.makedirs(directory)  # Create the directory if it doesn't exist
-
-    # Now save the file
-    savetxt(os.path.join(directory, 'harmonics.txt'), harmonic_data, fmt='%f')
+print(linspace(0.,20,50))
